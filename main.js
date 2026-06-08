4348bd9b1f37c13ca55417516c364685592304d6
Rama <ramirologiurato@gmail.comgit config --global user.name Ramagit statusgit config --global user.email ramirologiurato@gmail.com2>
2026-06-08 19:52:43 -0300

    Added a Score Counter with 5 matches until reset.

--- main.js
+++ main.js
@@ -2,8 +2,11 @@ const computerResult = document.querySelector("h1");
 const matchResult = document.querySelector("p");
 const humanHand = document.querySelector("input");
 const playBtn = document.querySelector("button");
+let scoreCount = document.getElementById("score");
 
 let computerResponse = "";
+let matchCountComputer = 0;
+let matchCountHuman = 0;
 
 function getComputerChoice() {
   const computerHand = Math.random();
@@ -17,20 +20,45 @@ function getComputerChoice() {
   return console.log(computerResponse);
 }
 
+function scoreDisplay() {
+  scoreCount.textContent = `${matchCountComputer} - ${matchCountHuman}`;
+}
+
+function matchCounting() {
+  if (matchCountComputer === 5) {
+    computerResult.textContent = "Machine Won The Match Series";
+    playBtn.textContent = "Replay";
+    matchCountComputer = 0;
+    matchCountHuman = 0;
+  } else if (matchCountHuman === 5) {
+    computerResult.textContent = "You Won The Match Series";
+    playBtn.textContent = "Replay";
+    matchCountComputer = 0;
+    matchCountHuman = 0;
+  } else {
+  }
+}
+
 function matchPlayed() {
   getComputerChoice();
   if (humanHand.value === "Rock" && computerResponse === "Paper") {
     computerResult.textContent = `You Lose Against ${computerResponse}`;
+    matchCountComputer += 1;
   } else if (humanHand.value === "Paper" && computerResponse === "Scissors") {
     computerResult.textContent = `You Lose Against ${computerResponse}`;
+    matchCountComputer += 1;
   } else if (humanHand.value === "Scissors" && computerResponse === "Rock") {
     computerResult.textContent = `You Lose Against ${computerResponse}`;
+    matchCountComputer += 1;
   } else if (humanHand.value === "Paper" && computerResponse === "Rock") {
     computerResult.textContent = `You Win Against ${computerResponse}`;
+    matchCountHuman += 1;
   } else if (humanHand.value === "Scissors" && computerResponse === "Paper") {
     computerResult.textContent = `You Win Against ${computerResponse}`;
+    matchCountHuman += 1;
   } else if (humanHand.value === "Rock" && computerResponse === "Scissors") {
     computerResult.textContent = `You Win Against ${computerResponse}`;
+    matchCountHuman += 1;
   } else if (humanHand.value === "Rock" && computerResponse === "Rock") {
     computerResult.textContent = `It's A ${computerResponse} Tie`;
   } else if (humanHand.value === "Paper" && computerResponse === "Paper") {
@@ -43,6 +71,8 @@ function matchPlayed() {
   } else {
   }
   matchResult.textContent = `${computerResponse}`;
+  scoreDisplay();
+  matchCounting();
 }
 
 playBtn.addEventListener("click", matchPlayed);
